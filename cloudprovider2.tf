5281943556849e266f884e5069a4d5e4922cb2af
Diego Pereira <diegopereira@Diegos-MacBook-Pro.local>
2021-10-29 16:44:16 -0300

    clear

--- cloudprovider2.tf
+++ cloudprovider2.tf
@@ -1,16 +1,9 @@
-
 resource "harness_cloudprovider_kubernetes" "demo" {
-  name = "${var.prefix}-kubernetes-sa"
+  name = "${var.prefix}-kubernetes"
 
   authentication {
-    service_account {
-      master_url = "https://10.10.10.10:443"
-      service_account_token_secret_name = "${var.prefix}-github-token"
-      ca_certificate_secret_name = "${var.prefix}-github-token"
-    }
+    delegate_selectors = ["k8s"]
   }
-
-
 
   usage_scope {
     environment_filter_type = "NON_PRODUCTION_ENVIRONMENTS"
